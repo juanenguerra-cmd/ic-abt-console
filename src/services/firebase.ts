a7a24c17edd9bd1a230d302b228f28b1cdb88f8e
juan.enguerra <juan.enguerra@gmail.com>
2026-03-08 03:05:38 +0000

    firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5qJ_qRmT78Ss718mVKLXll_1rPd306_A",
+  authDomain: "ic-abt-console-44321826-ea0eb.firebaseapp.com",
+  projectId: "ic-abt-console-44321826-ea0eb",
+  storageBucket: "ic-abt-console-44321826-ea0eb.firebasestorage.app",
+  messagingSenderId: "400859131849",
+  appId: "1:400859131849:web:a7c6421c22f7b7d314c218"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
