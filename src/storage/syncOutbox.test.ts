914fed0fa80e1f8599a5fd8b7156085a832e44db
juan.enguerra <juan.enguerra@gmail.com>
2026-03-09 15:41:46 +0000

    sync

--- src/storage/syncOutbox.test.ts
+++
@@ -1,9 +0,0 @@
-
-import { hasOutboxItems } from './syncOutbox';
-
-describe('Sync Outbox', () => {
-  it('should check if there are outbox items', async () => {
-    const result = await hasOutboxItems();
-    console.log('Sync status:', result ? 'Not all items are synced' : 'All items are synced');
-  });
-});
